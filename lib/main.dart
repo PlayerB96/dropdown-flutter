e01c21107677c6b87d328befb9e7f1dfba6774d4
Bryan <player.b.96@hotmail.com>
2021-10-25 23:42:59 -0500

    v4

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LocatorApp',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
@@ -35,14 +35,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _chosenValue;
   List<String> empresas = [
-    "REINO DE QUITO",
-    "TRANSPORSEL",
-    "QUITUMBE",
-    "TRANSALFA",
-    "COLECTRANS",
-    "NACIONAL",
-    "RAPITRANS"
-  ];
+        "REINO DE QUITO",
+        "TRANSPORSEL",
+        "QUITUMBE",
+        "TRANSALFA",
+        "COLECTRANS",
+        "NACIONAL",
+        "RAPITRANS"
+      ],
+      url = [
+        "https://nimbus.wialon.com/locator/606e95159c964a95aac201f5d3cbe6d5",
+        "https://nimbus.wialon.com/locator/45c3b367f8ef49a09699f39a77e6b037",
+        "https://nimbus.wialon.com/locator/93b33e6675ea4fc99894b984a05b8a63",
+        "https://nimbus.wialon.com/locator/0abb1d1e4a9d45918e86a7b82fcff5a5",
+        "https://nimbus.wialon.com/locator/4fe87c3d1a574162a6f240d5a0461eeb",
+        "https://nimbus.wialon.com/locator/5e61cc2c0d644c9f90f530210726056e",
+        "https://nimbus.wialon.com/locator/a189b56b42f94cd397a46416ea908a9a"
+      ];
   String value = 'REINO DE QUITO';
 
   @override
@@ -64,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   BoxShadow(color: Colors.white),
                 ],
               ),
-              child: DropdownButton(
+              child: DropdownButton<String>(
                 icon: Icon(
                   Icons.arrow_downward,
                   color: Colors.redAccent,
@@ -74,44 +83,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(color: Colors.black, fontSize: 18),
                 dropdownColor: Colors.white,
                 elevation: 6,
-               
-                items: [
-                  DropdownMenuItem(
-                    child: Text("Transporsel"),
-                    value:
-                        "https://nimbus.wialon.com/locator/606e95159c964a95aac201f5d3cbe6d5",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Rapitrans"),
-                    value:
-                        "https://nimbus.wialon.com/locator/45c3b367f8ef49a09699f39a77e6b037",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Quitumbe"),
-                    value:
-                        "https://nimbus.wialon.com/locator/93b33e6675ea4fc99894b984a05b8a63",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Transalfa"),
-                    value:
-                        "https://nimbus.wialon.com/locator/0abb1d1e4a9d45918e86a7b82fcff5a5",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Reino de Quito"),
-                    value:
-                        "https://nimbus.wialon.com/locator/4fe87c3d1a574162a6f240d5a0461eeb",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Colectrans"),
-                    value:
-                        "https://nimbus.wialon.com/locator/5e61cc2c0d644c9f90f530210726056e",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Nacional"),
-                    value:
-                        "https://nimbus.wialon.com/locator/a189b56b42f94cd397a46416ea908a9a",
-                  ),
-                ],
+                items: empresas.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                
                 hint: Text(
                   "Selecciona Empresa",
                   style: TextStyle(
@@ -119,12 +97,57 @@ class _MyHomePageState extends State<MyHomePage> {
                       fontSize: 18,
                       fontWeight: FontWeight.w600),
                 ),
-                onChanged: (String value) {
+                onChanged: (String a) {
                   setState(() {
-                    _chosenValue = value;
+                    _chosenValue = a;
+                    switch(a){
+                  case "REINO DE QUITO" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FirstRoute()),
+                    );
+                    break;
+                  case "TRANSPORSEL" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    );
+                    break;
+                    case "QUITUMBE" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdRoute()),
+                    );
+                    break;
+                  case "TRANSALFA" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FourRoute()),
+                    );
+                    break;
+                    case "COLECTRANS" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FiveRoute()),
+                    );
+                    break;
+                  case "NACIONAL" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SixRoute()),
+                    );
+                    break;
+                    case "RAPITRANS" :
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SevenRoute()),
+                    );
+                    break;
+                  
+                  
+              }
                   });
                 },
-                
               ),
             ),
             const SizedBox(height: 30),
@@ -175,17 +198,239 @@ class _FirstRouteState extends State<FirstRoute> {
   @override
   Widget build(BuildContext context) {
     return WebviewScaffold(
-        url: widget.url,
+        url: "https://nimbus.wialon.com/locator/606e95159c964a95aac201f5d3cbe6d5",
         withZoom: true,
         hidden: true,
         withLocalStorage: true,
         geolocationEnabled: true,
         appBar: AppBar(
             backgroundColor: Colors.teal,
-            title: Text("Regresar",
+            title: Text("Quito",
                 style: const TextStyle(fontWeight: FontWeight.bold))));
   }
 }
+
+class SecondRoute extends StatefulWidget {
+  SecondRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _SecondRouteState createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/45c3b367f8ef49a09699f39a77e6b037",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("Tranportel",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+
+class ThirdRoute extends StatefulWidget {
+  ThirdRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _ThirdRouteState createState() => _ThirdRouteState();
+}
+
+class _ThirdRouteState extends State<ThirdRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/93b33e6675ea4fc99894b984a05b8a63",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("QUITUMBE",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+
+class FourRoute extends StatefulWidget {
+  FourRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _FourRouteState createState() => _FourRouteState();
+}
+
+class _FourRouteState extends State<FourRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/0abb1d1e4a9d45918e86a7b82fcff5a5",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("TRANSALFA",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+
+class FiveRoute extends StatefulWidget {
+  FiveRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _FiveRouteState createState() => _FiveRouteState();
+}
+
+class _FiveRouteState extends State<FiveRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/4fe87c3d1a574162a6f240d5a0461eeb",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("RAPITRANS",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+
+class SixRoute extends StatefulWidget {
+  SixRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _SixRouteState createState() => _SixRouteState();
+}
+
+class _SixRouteState extends State<SixRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/5e61cc2c0d644c9f90f530210726056e",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("NACIONAL",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+class SevenRoute extends StatefulWidget {
+  SevenRoute({this.url});
+
+  final String url;
+  String value;
+
+  @override
+  _SevenRouteState createState() => _SevenRouteState();
+}
+
+class _SevenRouteState extends State<SevenRoute> {
+  Future<LocationPermission> permission = Geolocator.requestPermission();
+  Future<Position> position =
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: "https://nimbus.wialon.com/locator/a189b56b42f94cd397a46416ea908a9a",
+        withZoom: true,
+        hidden: true,
+        withLocalStorage: true,
+        geolocationEnabled: true,
+        appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("RAPITRANS",
+                style: const TextStyle(fontWeight: FontWeight.bold))));
+  }
+}
+
+
 
 class CustomGps {
   Future<Position> getCurrentPosition() async {
