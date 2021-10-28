b9923aff2617c6c81e20f61fc52ac9564debccde
Bryan <player.b.96@hotmail.com>
2021-10-27 23:22:57 -0500

    cambio

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LocatorApp',
+      title: 'Cambio',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
