c6e341e0e1572ae8f2d3631fbd69181d92531774
Bryan <player.b.96@hotmail.com>
2021-10-27 23:12:07 -0500

    a

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return VRouter(
-      title:"LocatorApp",
+      title:"Cambio",
       initialUrl: '/homepage',
       routes: [
         VWidget(
